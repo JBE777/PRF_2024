270b6fc3ac9653ef39930d81a7bcd9e508c22045
JBE777 <49010974+JBE777@users.noreply.github.com>
2024-02-15 17:28:43 -0700

    PRF_2024

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 ## Sistema de recomendación 
 
 https://jbe777-prf-2024-introduccion-nb4ywu.streamlit.app/
+
+https://jbe777-prf-2024-app-yatrer.streamlit.app/
--- app.py
+++ app.py
@@ -8,7 +8,7 @@ def load_data(data):
 def main():
     st.title("Suggestions")
     Rest_Mex = load_data("dataset_rm.csv")
-    menu = ["Home","States","Cities","Mexican Restaurants"]
+    menu = ["Home","States","Restaurants","Mexican Restaurants"]
 
     choice = st.sidebar.selectbox("Menu",menu)
     color = st.sidebar.color_picker("Color",value="#0000ff")
@@ -32,11 +32,11 @@ def main():
         fig.update_layout(mapbox_style="open-street-map")
         st.plotly_chart(fig)
     
-    elif choice == "Cities":
-        cities_list =  Rest_Mex["City"].unique().tolist()
-        selected_city = st.sidebar.selectbox("City", cities_list)
+    elif choice == "Restaurants":
+        restaurants_list =  Rest_Mex["Mexican restaurant"].unique().tolist()
+        selected_restaurant = st.multiselect("Restaurant", restaurants_list,default=["Taco Bell"])
         with st.expander("Data View"):
-            df = Rest_Mex[Rest_Mex["City"] == selected_city]
+            df = Rest_Mex[Rest_Mex["Mexican restaurant"].isin(selected_restaurant)]
             st.dataframe(df)
         fig = px.scatter_mapbox(df,lat="Latitude",lon="Longitude",hover_name="Mexican restaurant",hover_data=["Address","City","State","Stars","Sentiment"],
                                     color_discrete_sequence=[color],zoom=5,height=700)
@@ -44,8 +44,9 @@ def main():
         st.plotly_chart(fig)
 
     else:
-        st.subheader("Mexican Restaurants")
+        st.subheader("🥪_Mexican Restaurants")
 
 
 if __name__ == "__main__":
     main()
+ipts
